3630855606aaae34aa193f37ac118082c9ef31ad
Mike Wabst <mike.wabst@trusted.com>
2016-02-29 09:39:27 -0700

    Updated retailers.js, sync

--- assets/js/retailers.js
+++ assets/js/retailers.js
@@ -1,4 +1,5 @@
 /*global Transparency */
+/*global Throbber */
 
 // Retailers view controller
 
@@ -11,8 +12,8 @@ jQuery.fn.render = Transparency.jQueryPlugin;
       $countryDrop = $('#select-storelist-country'),
       $retailerListContainer = $('#retailers-listing'),
       $retailerCont = $('.retailer-container.render').remove(),
+      isLoading = true,
       brandIndex = [],
-      retailerIndex = [],
       selectedOption = {
         'brand' : 'all-brands',
         'country' : 'all-countries'
@@ -20,7 +21,7 @@ jQuery.fn.render = Transparency.jQueryPlugin;
       endpoints = {
         //All brands, all countries
         'all' : '/webservices/store/storelist',
-        //List all brands available in specific country: /store/manufacturer/name/<country>
+        //List all brands available in specific country: /store/manufacturer/names/<country>
         'countryHasBrands' : '/webservices/store/manufacturer/names/',
         //List of countries for a selected brand: store/countrieslist/<manufacturerId>
         'brandHasCountries' : '/webservices/store/countrieslist/',
@@ -34,22 +35,94 @@ jQuery.fn.render = Transparency.jQueryPlugin;
 
       //Load
       var init = function() {
+        showLoading(true);
         selectListener();
+        window.onpopstate = historyAction;
+        historyUpdate(selectedOption);
         getRetailerData(endpoints.all);
+      };
+
+      //Listener: Bind event listeners to dropdowns
+      var selectListener = function() {
+        $brandDrop.on('change historyActionEvt', filterUpdate);
+        $countryDrop.on('change historyActionEvt', filterUpdate);
+      };
+
+      //Dropdown change triggered, filter accordingly
+      var filterUpdate = function(e) {
+        var selValue = e.target.value,
+            filterType = e.target.dataset.type;
+
+        if (selValue.length) {
+          switch(filterType) {
+            case 'brand':
+              selectedOption.brand = selValue;
+              if (selectedOption.country === 'all-countries') {
+
+                if (selectedOption.brand === 'all-brands') {
+                  getRetailerData(endpoints.all, 'brandDropdown');
+                } else {
+                  getRetailerData(endpoints.brandAll+selectedOption.brand, 'brandDropdown');
+                }
+
+              } else {
+                // Update Country dropdown for specific value (e.g. 'Gucci-Watches' for countries available)
+                if (selectedOption.brand !== 'all-brands') {
+                  getRetailerData(endpoints.brandHasCountries+selectedOption.brand, 'brandDropdown');
+                } else {
+                  ////getRetailerData(endpoints.countryHasBrands+selectedOption.country, 'countryDropdown');
+                  getRetailerData(endpoints.countryAll+selectedOption.country, 'countryDropdown');
+                }
+              }
+              break;
+            case 'country':
+              selectedOption.country = selValue;
+              if (selectedOption.brand === 'all-brands') {
+                if (selectedOption.country === 'all-countries') {
+                  getRetailerData(endpoints.all, 'countryDropdown');
+                } else {
+                  // Update Brand dropdown for specific value (e.g. 'US' for brands in US)
+                  getRetailerData(endpoints.countryAll+selectedOption.country, 'countryDropdown');
+                }
+              } else {
+                // Update Brand dropdown for specific value
+                //getRetailerData(endpoints.countryHasBrands+selectedOption.country, 'countryDropdown');
+                if (selectedOption.country !== 'all-countries') {
+                  //Specific country and brand
+                  getRetailerData(endpoints.brandCountryDetail+selectedOption.brand+'/'+selectedOption.country);
+                } else {
+                  //All countries selected, brand selected
+                  getRetailerData(endpoints.brandAll+selectedOption.brand, 'brandDropdown');
+                }
+              }
+              break;
+          }
+        }
+
+        //Only update browser history if user selects option
+        if (e.type !== 'historyActionEvt') {
+          historyUpdate(selectedOption);
+        }
+
       };
 
       //Getter: GET request to endpoint with serial number, call results fn if successful
       var getRetailerData = function(webserviceUrl, isDropdown) {
         if (webserviceUrl && typeof webserviceUrl === 'string') {
+          if (!isLoading) {
+            showLoading(true);
+          }
           $.get(webserviceUrl)
           .done(function(data) {
-            if (isDropdown) {
+            if (isDropdown === 'brandDropdown' || isDropdown === 'countryDropdown') {
+              outputResults(data);
               updateDropdowns(data, isDropdown);
             } else {
               outputResults(data);
             }
           })
           .fail(function(xhr) {
+            showLoading(false);
             if (isDropdown) {
               outputResults([], isDropdown, 'fail');
             } else {
@@ -62,29 +135,35 @@ jQuery.fn.render = Transparency.jQueryPlugin;
       //After dropdowns data refreshed, update options
       var updateDropdowns = function(data, dropdownType, callResult) {
         if (data.length && typeof dropdownType === 'string') {
-          if (dropdownType === 'brandDropdown') {
-            //Update $countryDrop
-            var countryOptions = [];
-            countryOptions.push($countryDrop.children('option[attr="disabled"], option[value="all-countries"]'));
-            for (var j=0; j < data.length; j++) {
-              var countryOptionValue = data[j].countryCode,
+          switch (dropdownType) {
+            case 'brandDropdown':
+              //Update country dropdown
+              var countryOptions = [];
+              countryOptions.push($countryDrop.children('option[disabled="disabled"], option[value="all-countries"]'));
+              for (var j=0; j < data.length; j++) {
+                var countryOptionValue = data[j].country,
                   countryOptionTxt = data[j].countryName,
                   countryOptionResult = '<option value="' + countryOptionValue + '">' + countryOptionTxt + '</option>';
+                if (countryOptions.indexOf(countryOptionResult) === -1 && countryOptionValue) {
                   countryOptions.push(countryOptionResult);
-            }
-            $countryDrop.html(countryOptions);
-          }
-          if (dropdownType === 'countryDropdown') {
-            //Update $brandDrop
-            var brandOptions = [];
-            brandOptions.push($brandDrop.children('option[attr="disabled"], option[value="all-brands"]'));
-            for (var k=0; k < data.length; k++) {
-              var brandOptionValue = data[[k]].brandUniqueName,
-                  brandOptionTxt = data[[k]].brandName,
-                  brandOptionResult = '<option value="' + brandOptionValue + '">' + brandOptionTxt + '</option>';
+                }
+              }
+              $countryDrop.html(countryOptions);
+              break;
+            case 'countryDropdown':
+              //Update brand dropdown
+              var brandOptions = [];
+              brandOptions.push($brandDrop.children('option[disabled="disabled"], option[value="all-brands"]'));
+              for (var k=0; k < data.length; k++) {
+                var brandOptionValue = data[[k]].brandUniqueName,
+                    brandOptionTxt = data[[k]].brandName,
+                    brandOptionResult = '<option value="' + brandOptionValue + '">' + brandOptionTxt + '</option>';
+                if (brandOptions.indexOf(brandOptionResult) === -1 && brandOptionValue) {
                   brandOptions.push(brandOptionResult);
-            }
-            $brandDrop.html(brandOptions);
+                }
+              }
+              $brandDrop.html(brandOptions);
+              break;
           }
         }
       };
@@ -94,7 +173,6 @@ jQuery.fn.render = Transparency.jQueryPlugin;
         if (data.length && typeof jQuery.fn.render === 'function') {
           //Reset (important when dropdowns change)
           brandIndex = [];
-          retailerIndex = [];
           $retailerListContainer.empty();
 
           //Begin Transparency.js template decorators, massages data for templates
@@ -102,7 +180,6 @@ jQuery.fn.render = Transparency.jQueryPlugin;
             var $logoEle = $(params.element);
             var imgBasePath = $logoEle.attr('data-imgpath');
             if (this.brandLogo !== null) {
-              //TODO: Must move this out into abstract config url
                 return '<img src="' + imgBasePath + this.brandLogo + '" alt="' + this.brandName + '" />';
             } else {
               return '<h2>' + this.brandName + '</h2>';
@@ -145,14 +222,10 @@ jQuery.fn.render = Transparency.jQueryPlugin;
             }
 
             //Prevent dupes
-            $listingEle.find('.retailer').attr('data-retailer', this.locationUniqueName);
-
-            if (retailerIndex.indexOf(this.locationUniqueName) === -1) {
-              retailerIndex.push(this.locationUniqueName);
-              if (brandIndex.indexOf(this.brandUniqueName) === -1) {
-                brandIndex.push(this.brandUniqueName);
-                $listingEle.attr('data-brand', this.brandUniqueName);
-              } else {
+            if (brandIndex.indexOf(this.brandUniqueName) === -1) {
+              brandIndex.push(this.brandUniqueName);
+              $listingEle.attr('data-brand', this.brandUniqueName);
+            } else {
                 var $groupEle = $listingEle.parent().find('div.store-list[data-brand="' + this.brandUniqueName + '"]');
                 if ($groupEle.find('.retailer-wrap[data-country="' + this.countryName + '"]').length) {
                   if ($groupEle.children('.retailer-wrap').find('.state-container[data-state="' + this.state + '"]').length) {
@@ -169,23 +242,19 @@ jQuery.fn.render = Transparency.jQueryPlugin;
                   $groupEle.append($listingEle.find('.retailer-wrap'));
                   $listingEle.remove();
                 }
-              }
-            } else {
-              $listingEle.remove();
             }
-
 
           };
 
-          //Transparency.js render directives, see above
+          //Transparency.js render data directives, see above
           var directives = {
-            logo: {
+            'logo': {
               html: logoDecorator
             },
-            country: {
+            'country': {
               text: countryDecorator
             },
-            retailerName: {
+            'retailerName': {
               html: retailerDecorator
             },
             'retailer-wrap' : {
@@ -201,6 +270,8 @@ jQuery.fn.render = Transparency.jQueryPlugin;
 
           //Clone new listing element, render data with Transparency.js
           var $brandListing = $retailerCont.clone().render(data, directives).removeClass('render hide');
+          //Stop the loading spinner
+          showLoading(false);
           $brandListing.find('.store-list').first().removeClass('single-line');
           $retailerListContainer.append($brandListing);
           //End Transparency.js templating logic
@@ -212,44 +283,42 @@ jQuery.fn.render = Transparency.jQueryPlugin;
 
       };
 
-      //Dropdown change triggered, filter accordingly
-      var filterUpdate = function(e) {
-        var selValue = e.target.value,
-          filterType = e.target.dataset.type;
-
-          if (selValue.length && filterType === 'brand') {
-            selectedOption.brand = selValue;
-            if (selectedOption.country === 'all-countries') {
-              //update Country dropdown
-              getRetailerData(endpoints.brandHasCountries+selValue, 'brandDropdown');
-              getRetailerData(endpoints.brandAll+selValue);
-            }
-          }
-
-          if (selValue.length && filterType === 'country') {
-            selectedOption.country = selValue;
-            if (selectedOption.brand === 'all-brands') {
-              //update Brand dropdown
-              selectedOption.country = selValue;
-              getRetailerData(endpoints.countryHasBrands+selValue, 'countryDropdown');
-              getRetailerData(endpoints.countryAll+selValue);
-            }
-          }
-
-          if (selectedOption.brand === 'all-brands' && selectedOption.country === 'all-countries') {
-            getRetailerData(endpoints.all);
-          } else if (selectedOption.brand !== 'all-brands' && selectedOption.country !== 'all-countries') {
-            getRetailerData(endpoints.brandCountryDetail+selectedOption.brand+'/'+selectedOption.country);
-          } else {
-            getRetailerData(endpoints.brandAll+selectedOption.brand);
-          }
-
+      //Push selection to history state
+      var historyUpdate = function(selectionObj) {
+        if (typeof selectionObj === 'object') {
+          var selectionData = JSON.stringify(selectionObj);
+          window.history.pushState(selectionData, 'ALL AUTHORIZED RETAILERS', window.location.pathname);
+        }
       };
 
-      //Listener: Bind event listeners to dropdowns
-      var selectListener = function() {
-        $brandDrop.on('change', filterUpdate);
-        $countryDrop.on('change', filterUpdate);
+      //Handle onpopstate event for history actions
+      var historyAction = function(e) {
+         var selectionRestore = JSON.parse(e.state);
+         $brandDrop.val(selectionRestore.brand).trigger('historyActionEvt');
+         $countryDrop.val(selectionRestore.country).trigger('historyActionEvt');
+      };
+
+      //Show or hide Throbber loading indicator
+      var showLoading = function(bool) {
+        isLoading = typeof bool === 'boolean' ? bool : false;
+        var $loadingEle = $('.smallThrobberLoading'),
+            $resultContainer = $('.retailer-container');
+        if (typeof Throbber === 'function' && $loadingEle.length) {
+          var throbber = Throbber({
+            color: 'black',
+            padding: 30,
+            size: 32,
+            fade: 200,
+          });
+          if (isLoading) {
+            $resultContainer.addClass('hide');
+            $loadingEle.removeClass('hide');
+            throbber.appendTo($loadingEle[0]).start();
+          } else {
+            $loadingEle.addClass('hide').find('canvas').remove();
+            $resultContainer.removeClass('hide');
+          }
+        }
       };
 
       init();
