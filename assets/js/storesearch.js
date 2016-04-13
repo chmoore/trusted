b7359364a9d6ecea7dc576ca33b83977c82fd277
Mike Wabst <mike.wabst@trusted.com>
2016-04-13 12:17:28 -0600

    WIP storesearch

--- assets/js/storesearch.js
+++ assets/js/storesearch.js
@@ -1,25 +1,82 @@
 // Store search view
 
 (function ($) {
-  $(document).ready(function() {
+  $(document).ready(function () {
     var $brandFilters = $('#brand-filters'),
-    $brandFiltersCont = $('#brand-filters-cont'),
-    $selectedBrands = $('<ul class="brands-selected" />'),
-    $brandsCont = $('<ul class="brands-continued" />'),
-    $selectedEle = $('.selectedBrand');
+    $toggleBrandsEle = $('<li class="show-toggle"><a class="blue-link" id="toggleMoreBrands" href="javascript:;">+ Show More</a></li>'),
+    $selectedBrand = $('.selectedBrand'),
+    $formReset = $('#productListReset'),
+    $resultLimitDropdowns = $('.searchLimitResults'),
+    $searchSortBy = $('#search-sortBy'),
+    numPerPageInt = 15,
+    isToggled = false,
+    intialRun = true;
 
-    var filterUpdate = function () {
-      $brandFiltersCont.prepend($selectedBrands).append($brandsCont);
-      $brandFilters.find($selectedEle).each(function () {
-        $selectedBrands.prepend($(this));
-      });
-      $brandFilters.not($selectedEle).each(function () {
-        $brandsCont.prepend($(this));
+
+    var filterToggle = function () {
+      var controls = '.selectedBrand, .show-toggle';
+      var toggleText = isToggled ? '+ Show More': '- Show Less';
+      var $selectedBrands = $brandFilters.find('.selectedBrand');
+      var $toggleBrands = $('#toggleMoreBrands');
+      if (intialRun) {
+        //initial stuff
+        intialRun = false;
+        $brandFilters.find('li').not(controls).addClass('hide');
+      } else {
+        if ($brandFilters.hasClass('view-all')) {
+          $brandFilters.find('li').not(controls).toggleClass('hide');
+          $brandFilters.prepend($selectedBrands);
+          isToggled = !isToggled;
+          $toggleBrands.text(toggleText);
+        }
+      }
+    };
+
+    var checkBoxUpdate = function () {
+      $(this).parent('li').addClass('selectedBrand');
+    };
+
+    var resetForm = function () {
+      $brandFilters.find('input').removeAttr('checked');
+    };
+
+    var mirrorDropdowns = function() {
+      $resultLimitDropdowns.change(function(e) {
+        $resultLimitDropdowns.val(this.value);
+        numPerPageInt = this.value;
       });
     };
 
+    var updateSearch = function (paramToSet) {
+      var newVal = function() {
+        switch(paramToSet) {
+          case 'numPerPage' :
+            return numPerPageInt;
+          case 'sortBy' :
+            return $searchSortBy.val();
+      }}();
+      if ($.query) {
+        console.log('works', paramToSet, newVal);
+        var updateSearch = $.param($.query.set(paramToSet, newVal).keys); window.location.search = '?' + updateSearch;
+      } else {
+        console.log('Failed to load jQuery.query plugin');
+      }
+    };
+
+    var bindThem = function () {
+      var $brandFilters = $('#brand-filters');
+      var $toggleBrands = $('#toggleMoreBrands');
+      $brandFilters.find('input').on('change', checkBoxUpdate);
+      $toggleBrands.on('click',  filterToggle);
+      $formReset.on('click', resetForm);
+      $resultLimitDropdowns.on('change', mirrorDropdowns);
+      $searchSortBy.on('change', updateSearch('sortBy'));
+    };
+
     var init = function () {
-      filterUpdate();
+      $brandFilters.append($toggleBrandsEle);
+      filterToggle();
+      bindThem();
     };
 
     init();
