49e953d0b87333e18a443619b7131c86942cded4
Ray Robertson <ray.robertson@trusted.com>
2016-04-13 11:49:06 -0400

    Added product.js File for Product.html

---
+++ assets/js/product.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+  $('#retailermodal').formValidation({
+    framework: 'bootstrap',
+    excluded: ':disabled, :hidden, :not(:visible)',
+    icon: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      fname: {
+        validators: {
+          notEmpty: {
+            message: 'Your first name is required'
+          }
+        }
+      },
+      lname: {
+        validators: {
+          notEmpty: {
+            message: 'Your last name is required'
+          }
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'Your email address is required'
+          },
+          regexp: {
+            regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
+            message: 'The value is not a valid email address'
+          }
+        }
+      },
+      message: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter a message for Trusted'
+          },
+        }
+      }
+    }
+  });
+
+  $("#zoom").elevateZoom({gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: false});
+  $('#zoom').ready(function(){
+    if($(window).width()>768){
+      $.removeData($(this), 'elevateZoom');
+      $('.zoomContainer').remove();
+      $('img.zoom').removeData('elevateZoom');
+      $('.zoomWrapper img.zoom').unwrap();
+      $('.zoomContainer').remove();
+    }
+  });
+
+});
