37b6d74ce60e24a4a6c43c735c474f356f99e0b2
Mike Wabst <mike.wabst@trusted.com>
2016-04-16 11:32:57 -0600

    Sync common js changes, added sidr

--- assets/js/productlist.js
+++ assets/js/productlist.js
@@ -148,6 +148,22 @@
       filterToggle();
       paginateResults();
       bindThem();
+      $('#responsive-menu-button').sidr({
+        name: 'sidr-main',
+        source: '#side-filters',
+        renaming: false
+      });
+      $('body').swipe( {
+        //Single swipe handler for left swipes
+        swipeLeft: function () {
+            $.sidr('close', 'sidr-main');
+        },
+        swipeRight: function () {
+            $.sidr('open', 'sidr-main');
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold: 45
+      });
     };
 
     init();
