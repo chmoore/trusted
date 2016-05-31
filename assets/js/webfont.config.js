a817a5de040973fd916f01fa7b6e5441bca4eae7
Mike Wabst <mike.wabst@trusted.com>
2016-05-31 14:46:52 -0600

    web font work

---
+++ assets/js/webfont.config.js
@@ -0,0 +1,19 @@
+/*global WebFont */
+
+var trustedAssetsURL = trustedAssetsURL || '';
+
+WebFont.load({
+  google: {
+    families: ['Noto+Serif:400,700,400italic,700italic']
+  },
+  custom: {
+    families: [
+      'FontAwesome',
+      'gotham_htfbold'
+    ],
+    urls: [
+      '//opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css',
+      trustedAssetsURL + 'assets/css/fonts.css'
+    ],
+  }
+});
