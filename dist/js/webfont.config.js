b9946b2a10996daa13d27213395a1fa21da21b3b
Mike Wabst <mike.wabst@trusted.com>
2016-06-07 09:11:32 -0600

    Adjusting webfont config js for all templates

---
+++ dist/js/webfont.config.js
@@ -0,0 +1,20 @@
+/*global WebFont */
+/*global "https://localhost/images/static35/global/" */
+
+var trustedAssetsURL = 'http://chmoore.github.io/trusted/' || '';
+
+WebFont.load({
+  google: {
+    families: ['Noto+Serif:400,700,400italic,700italic']
+  },
+  custom: {
+    families: [
+      'FontAwesome',
+      'gotham_htfbold'
+    ],
+    urls: [
+      '//opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css',
+      trustedAssetsURL + 'assets/css/fonts.css'
+    ]
+  }
+});
